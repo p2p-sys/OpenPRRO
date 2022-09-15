2b673fefba689dd934385d32187114fec2fdb37a
p2p-sys <37340795+p2p-sys@users.noreply.github.com>
2022-09-15 15:54:36 +0200

    Update config.py
    Added TIMEZONE
    Code review

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
-import os
-
-CONFIG_PATH = os.path.abspath(os.path.dirname(__file__))
 TESTING = False
 DEBUG = False
 
@@ -16,7 +13,6 @@ DATABASE_HOST = '127.0.0.1'
 SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{}:{}@{}/{}?charset=utf8mb4".format(DATABASE_USER, DATABASE_PWD, DATABASE_HOST,
                                                                             DATABASE_NAME)
 
-# Ext
 SECRET_KEY = 's0318f3b-a802-410d-bb0f-8ff73b3b4768'
 WTF_CSRF_ENABLED = True
 CSRF_ENABLED = True
@@ -26,10 +22,11 @@ ENABLE_ADMIN_PANEL = True
 
 NO_PWD_CHECK = False
 
-# Custom
 HANDLE_ERRORS = True
 SESSION_EXPIRATION_SECONDS = 5400
 WTF_CSRF_TIME_LIMIT = 5400
 
 REMOTE_SIGNER_SERVER = '127.0.0.1:3100'
 REMOTE_SIGNER_SERVER_KEY = '4d505eacff74a1637ed6ce85ea38284b739083b87855d0dcd77d24c42774cb90'
+
+TIMEZONE = 'Europe/Kiev'
